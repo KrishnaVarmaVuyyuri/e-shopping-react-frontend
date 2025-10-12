ea4a85b3d00d36a630ae097be40a1c0d83664d55
KrishnaVarmaVuyyuri <your-email@example.com>
2025-10-12 15:01:26 +0530

    added the orders page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import CheckOut from './pages/CheckOut/CheckOut'
+import Orders from './pages/Orders/Orders'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/checkout' element={<CheckOut/>}/>
+      <Route path='/orders' element={<Orders/>}/>
     </Routes>
     </>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
-// import { Link } from 'react-router';
+import { Link } from 'react-router';
 import './header.css';
 
 export default function Header() {
   return (
     <div className="header">
       <div className="left-section">
-        {/* <Link to="/" className="header-link">
+        <Link to="/" className="header-link">
           <img className="logo"
             src="images/logo-white.png" />
           <img className="mobile-logo"
             src="images/mobile-logo-white.png" />
-        </Link> */}
+        </Link>
       </div>
 
       <div className="middle-section">
@@ -22,16 +22,16 @@ export default function Header() {
       </div>
 
       <div className="right-section">
-        {/* <Link className="orders-link header-link" to="/orders">
+        <Link className="orders-link header-link" to="/orders">
 
           <span className="orders-text">Orders</span>
-        </Link> */}
+        </Link>
 
-        {/* <Link className="cart-link header-link" to="/checkout">
+        <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">3</div>
           <div className="cart-text">Cart</div>
-        </Link> */}
+        </Link>
       </div>
     </div>
   );
